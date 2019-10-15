61c428c22f455d2126cea660b48aed075113f1eb
SonnyLuong <sonnyluong9@yahoo.com>
2019-10-14 21:57:25 -0700

    Create InitialsCheck.java

---
+++ InitialsCheck.java
@@ -0,0 +1,20 @@
+public class InitialsCheck
+{
+   /**
+      Gets the initials of this name
+      @params first, middle, and last names
+      @return a string consisting of the first character of the first, middle,
+      and last name
+   */
+   public static String getInitials(String one, String two, String three)
+   {
+
+      // your work here
+      char x = one.charAt(0);
+      char y = two.charAt(0);
+      char z = three.charAt(0);
+      String ans = x + "" + y + "" + z;
+      return ans;
+
+   }
+}
